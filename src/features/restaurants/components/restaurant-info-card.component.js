755510b9e05ce525b6c29fdc512509365419a732
Weifang <wazup2you@gmail.com>
2022-03-08 00:43:42 +0100

    Implemting styled component

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,9 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
-import { styleProps } from "react-native-web/dist/cjs/modules/forwardedProps";
 
-export const RestaurantInfo = ({ restaurant = {} }) => {
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled(Text)`
+  padding: 16px
+  color: red;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Dodo Restaurant",
     icon,
@@ -17,15 +31,9 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
   } = restaurant;
 
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-      <Text style={styles.title}>{name}</Text>
-    </Card>
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
   );
 };
-
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-  title: { padding: 16 },
-});
